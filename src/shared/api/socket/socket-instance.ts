97a526a6f2274e684611010104e8b0fd5232462c
Sqpine <opka.ru.ua@gmail.com>
2023-02-17 21:42:22 +0200

    remove comments

--- src/shared/api/socket/socket-instance.ts
+++ src/shared/api/socket/socket-instance.ts
@@ -5,24 +5,6 @@ import { Context } from "./types";
 
 export const context: Context = { socket: null };
 
-// function creatChannel(username: string) {
-// socket.on("connected", (players) => {
-//   debugger;
-//   console.log("players: ", players);
-// });
-// socket.on("opponent_made_choice", (players) => {
-//   console.log("players: ", players);
-// });
-// socket.emit("get_players");
-// socket.on("players_received", (players) => {
-//   debugger;
-//   console.log("players: ", players);
-// });
-// socket.on("game_finished", (players) => {
-//   console.log("players: ", players);
-// });
-// }
-
 export const SocketAPI = {
   start(username: string) {
     if (context.socket) {
@@ -34,10 +16,6 @@ export const SocketAPI = {
         username,
       },
       transports: ["websocket"],
-    });
-    context.socket.on("connected", (players) => {
-      debugger;
-      console.log("players: ", players);
     });
   },
   stop() {
